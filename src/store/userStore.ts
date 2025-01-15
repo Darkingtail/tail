cb14dba6b5b1027b62fae50a172f8eccdc576683
Darkingtail <1603774836@qq.com>
2025-01-15 21:26:30 +0800

    feat(store): user store

--- src/store/userStore.ts
+++ src/store/userStore.ts
@@ -3,13 +3,17 @@ import { useMutation } from "@tanstack/react-query";
 import { useNavigate } from "react-router-dom";
 // https://github.com/pmndrs/zustand
 import { create } from "zustand";
-import { createJSONStorage, persist } from "zustand/middleware";
+import { createJSONStorage, devtools, persist } from "zustand/middleware";
 
 import userService, { type SignInReq } from "@/api/services/userService";
 
-import { toast } from "sonner";
 import type { UserInfo, UserToken } from "#/entity";
 import { StorageEnum } from "#/enum";
+
+import { t } from "@/locales/i18n";
+
+// https://sonner.emilkowal.ski/
+import { toast } from "sonner";
 
 const { VITE_APP_HOMEPAGE: HOMEPAGE } = import.meta.env;
 
@@ -25,30 +29,32 @@ type UserStore = {
 };
 
 const useUserStore = create<UserStore>()(
-	persist(
-		(set) => ({
-			userInfo: {},
-			userToken: {},
-			actions: {
-				setUserInfo: (userInfo) => {
-					set({ userInfo });
+	devtools(
+		persist(
+			(set) => ({
+				userInfo: {},
+				userToken: {},
+				actions: {
+					setUserInfo: (userInfo) => {
+						set({ userInfo });
+					},
+					setUserToken: (userToken) => {
+						set({ userToken });
+					},
+					clearUserInfoAndToken() {
+						set({ userInfo: {}, userToken: {} });
+					},
 				},
-				setUserToken: (userToken) => {
-					set({ userToken });
-				},
-				clearUserInfoAndToken() {
-					set({ userInfo: {}, userToken: {} });
-				},
-			},
-		}),
-		{
-			name: "userStore", // name of the item in the storage (must be unique)
-			storage: createJSONStorage(() => localStorage), // (optional) by default, 'localStorage' is used
-			partialize: (state) => ({
-				[StorageEnum.UserInfo]: state.userInfo,
-				[StorageEnum.UserToken]: state.userToken,
 			}),
-		},
+			{
+				name: "userStore", // name of the item in the storage (must be unique)
+				storage: createJSONStorage(() => localStorage), // (optional) by default, 'localStorage' is used
+				partialize: (state) => ({
+					[StorageEnum.UserInfo]: state.userInfo,
+					[StorageEnum.UserToken]: state.userToken,
+				}),
+			},
+		),
 	),
 );
 
@@ -65,7 +71,7 @@ export const useUserActions = () => useUserStore((state) => state.actions);
 export const useSignIn = () => {
 	const navigatge = useNavigate();
 	const { setUserToken, setUserInfo } = useUserActions();
-
+	// tanstack/react-query useMutation
 	const signInMutation = useMutation({
 		mutationFn: userService.signin, // api function
 	});
@@ -74,10 +80,10 @@ export const useSignIn = () => {
 		try {
 			const res = await signInMutation.mutateAsync(data);
 			const { user, accessToken, refreshToken } = res;
-			setUserToken({ accessToken, refreshToken });
-			setUserInfo(user);
-			navigatge(HOMEPAGE, { replace: true });
-			toast.success("Sign in success!", { closeButton: true });
+			setUserToken({ accessToken, refreshToken }); // set token
+			setUserInfo(user); // set user info
+			navigatge(HOMEPAGE, { replace: true }); // navigate to homepage
+			toast.success(t("sys.login.loginSuccessTitle"), { closeButton: true });
 		} catch (err) {
 			toast.error(err.message, {
 				position: "top-center",
