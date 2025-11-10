139ff041ed43b2680af5f4336bb8da43c9e279a7
Darkingtail <1603774836@qq.com>
2025-11-10 23:04:55 +0800

    fix(mall4r): upd sidebar

--- apps/mall4r/src/layout/sidebar.tsx
+++ apps/mall4r/src/layout/sidebar.tsx
@@ -1,13 +1,36 @@
 import useRouteStore from "@/store/routerStore";
 import useSettingStore from "@/store/settingStore";
 import { ThemeMode } from "@/types/enum";
-import { Layout, theme } from "antd";
+import { Layout, Menu, type MenuProps, theme } from "antd";
+import { useEffect, useState } from "react";
+import type { RouteObject } from "react-router-dom";
 
 export default function SideBar() {
 	const themeMode = useSettingStore((state) => state.settings.themeMode);
 	const routes = useRouteStore((state) => state.routes);
+	const [menu, setMenu] = useState([]);
 	const { token } = theme.useToken();
 	console.log("routes:", routes);
+
+	const onClick: MenuProps["onClick"] = (e) => {
+		console.log("click", e);
+	};
+
+	useEffect(() => {
+		const generateMenu = (routes: RouteObject[]) => {
+			return routes.map((i) => {
+				if (i.children && i.children.length > 0) {
+					i.children = generateMenu(i.children);
+				}
+				return {
+					...i,
+					title: i.handle.title,
+				};
+			});
+		};
+		console.log("generateMenu(routes):", generateMenu(routes));
+		// setMenu( generateMenu(routes))
+	});
 
 	return (
 		<Layout.Sider
@@ -17,7 +40,7 @@ export default function SideBar() {
 				background: token.colorBgElevated,
 			}}
 		>
-			12312313131
+			<Menu onClick={onClick} mode="vertical" items={menu} />
 		</Layout.Sider>
 	);
 }
